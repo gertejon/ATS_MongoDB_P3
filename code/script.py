dfea288c4f700d4f05f2369714496cec4037fd27
haroldgonzales98 <haroldandre.gonzales@autonoma.cat>
2022-05-15 22:14:21 +0200

    3 primeros ejercicios

--- code/script.py
+++ code/script.py
@@ -1,12 +1,33 @@
 import json
 from pymongo import MongoClient
-cli = MongoClient('localhost', 27017)
-db = cli['test']
-coll = db['companies']
 
-with open('companies/companies.json', encoding="utf8") as f:
-    file_data = json.load(f)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["test"]
+coll = db["companies"]
 
-coll.insert_many(file_data)
+def print_value_key(collection, field, key, value):     #funcion que ayuda a imprimir el valor de una field con una condicion de key
+    base = collection.find({})
+    for d in base:
+        if d[field] == key:
+            valor = str(d[value])
+            print("El valor de "+value+" es "+valor)
 
-cli.close()
+#EJERCICIO 1
+
+# coll.update_many({"name": 'MeetMoi'}, {"$inc": {"number_of_employees": 1}})
+# print_value_key(coll, "name", "MeetMoi", "number_of_employees")
+
+#EJERCICIO 2
+
+# count = str(coll.count_documents({"founded_year" : 2000}))
+# print("El número de compañias fundado el año 2000 es de "+count)
+
+#EJERCICIO 3
+
+# count = str(len(coll.distinct("category_code")))
+# print("Existen "+count+" valores distintos en el campo category_code")
+
+#EJERCICIO 4
+
+
+client.close()
